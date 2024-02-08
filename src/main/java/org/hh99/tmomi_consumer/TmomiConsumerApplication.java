b4cf0a7452007d038bb0a0d1b0717651782d151e
heroq <96118954+heroq@users.noreply.github.com>
2024-02-08 18:19:11 +0900

    Feat: init

---
+++ src/main/java/org/hh99/tmomi_consumer/TmomiConsumerApplication.java
@@ -0,0 +1,13 @@
+package org.hh99.tmomi_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmomiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmomiConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hh99/tmomi_consumer/TmomiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hh99.tmomi_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmomiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

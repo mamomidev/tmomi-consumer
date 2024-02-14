5ecb2a3b0f105b0b66eaa6b159b79d0af0dc3b6c
heroq <96118954+heroq@users.noreply.github.com>
2024-02-14 23:57:35 +0900

    Feat: 카프카 pub, sub 테스트

--- src/main/java/org/hh99/tmomi_consumer/TmomiConsumerApplication.java
+++ src/main/java/org/hh99/tmomi_consumer/TmomiConsumerApplication.java
@@ -2,12 +2,18 @@ package org.hh99.tmomi_consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class TmomiConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TmomiConsumerApplication.class, args);
+	}
+
+	@KafkaListener(id = "Consumer3", topics = "Topic2", groupId = "1")
+	public void listen(String in) {
+		System.out.println(in);
 	}
 
 }

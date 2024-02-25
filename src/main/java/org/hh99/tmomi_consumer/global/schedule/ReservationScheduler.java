16d97595701f884a029771166dc09f52e1113d6f
TaeHoon0 <kimtahwn@naver.com>
2024-02-25 15:18:28 +0900

    Refactor: 대기열 수정

--- src/main/java/org/hh99/tmomi_consumer/global/schedule/ReservationScheduler.java
+++ src/main/java/org/hh99/tmomi_consumer/global/schedule/ReservationScheduler.java
@@ -12,14 +12,14 @@ import java.util.Set;
 @Component
 @RequiredArgsConstructor
 public class ReservationScheduler {
-    private static final long delay = 5000L;
+    private static final long delay = 20000L;
     private final ReservationQueue reservationQueue;
     private final EmitterService emitterService;
 
     @Scheduled(fixedRate = delay)
     public void reservationScheduler() {
         // 입장처리할 사용자들
-        Set<ReservationDto> enterQueue = reservationQueue.getQueue(5L);
+        Set<ReservationDto> enterQueue = reservationQueue.getQueue(4L);
 
         enterQueue.parallelStream().forEach(reservationDto -> {
             emitterService.sendSeatListToClient(reservationDto);
@@ -30,7 +30,7 @@ public class ReservationScheduler {
         // 나머지 대기열 가져오기, 사용자에게 대기 순위 보내주기
         Set<ReservationDto> waitQueue = reservationQueue.getQueue(-1L);
         waitQueue.parallelStream().forEach(reservationDto -> {
-            Long rank = reservationQueue.getRank(reservationDto);
+            Long rank = reservationQueue.getRank(reservationDto) + 1;   // 0순위 부터 시작
             System.out.println("대기 인원 : " + reservationDto.getEmail() + " : " + rank);
             emitterService.sendWaitNumberToClient(reservationDto, rank);
         });

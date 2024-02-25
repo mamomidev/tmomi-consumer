1756cce1f7f61ccda431502f9522e6e67916253f
TaeHoon0 <kimtahwn@naver.com>
2024-02-26 00:39:30 +0900

    Fix: tmomi_consumer security 수정

--- src/main/java/org/hh99/tmomi_consumer/global/config/SecurityConfig.java
+++ src/main/java/org/hh99/tmomi_consumer/global/config/SecurityConfig.java
@@ -1,42 +1,14 @@
 package org.hh99.tmomi_consumer.global.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Import({org.hh99.tmomi.global.config.SecurityConfig.class, org.hh99.tmomi.global.jwt.JwtTokenProvider.class})
 public class SecurityConfig {
-
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity
-			.httpBasic(HttpBasicConfigurer::disable)
-			.csrf(CsrfConfigurer::disable)
-			.sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.authorizeHttpRequests(authorize ->
-				authorize
-					.requestMatchers("/api/v1/sse-connection")
-					.permitAll()
-					.anyRequest()
-					.authenticated());
-		return httpSecurity.build();
-
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
 
 }

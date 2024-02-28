099c1aac320df640b76dce1af514c81d7b7aca4a
TaeHoon0 <kimtahwn@naver.com>
2024-02-29 04:31:53 +0900

    Fix : redisConfig 수정

--- src/main/java/org/hh99/tmomi_consumer/global/config/RedisConfig.java
+++ src/main/java/org/hh99/tmomi_consumer/global/config/RedisConfig.java
@@ -6,9 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -16,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
-@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
 
 	private final RedisProperties redisProperties;

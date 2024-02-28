773ff1ee9ced30103171c2c72e9a1c69aa067815
TaeHoon0 <kimtahwn@naver.com>
2024-02-29 04:28:52 +0900

    Fix : securityConfig 수정

--- src/main/java/org/hh99/tmomi_consumer/global/config/RedisConfig.java
+++ src/main/java/org/hh99/tmomi_consumer/global/config/RedisConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -14,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
 
 	private final RedisProperties redisProperties;

c9c3b0881c9eb9756935a6bd3c250ce729c59429
heroq <96118954+heroq@users.noreply.github.com>
2024-03-04 08:09:12 +0900

    Fix: 스케줄러 병렬 처리 10개

--- src/main/java/org/hh99/tmomi_consumer/TmomiConsumerApplication.java
+++ src/main/java/org/hh99/tmomi_consumer/TmomiConsumerApplication.java
@@ -3,13 +3,10 @@ package org.hh99.tmomi_consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @EntityScan(basePackages = {"org.hh99.tmomi"})
 @EnableRedisRepositories(basePackages = {"org.hh99.tmomi.global.redis"})
 @EnableJpaRepositories(basePackages = {"org.hh99.tmomi"})
